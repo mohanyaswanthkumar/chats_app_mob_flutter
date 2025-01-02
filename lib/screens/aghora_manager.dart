dcb90464df8aa66c3ef37dcdeed91b377afc9c63
mohanyaswanthkumar <110449846+mohanyaswanthkumar@users.noreply.github.com>
2025-01-02 21:17:45 +0530

    Update aghora_manager.dart

--- lib/screens/aghora_manager.dart
+++ lib/screens/aghora_manager.dart
@@ -1,7 +1,7 @@
 import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 
 class AgoraManager {
-  static const String appId = 'ba960aaa02ef4be7adeb3a12cfe3b15f'; // Replace with your Agora App ID
+  static const String appId = 'YOUR-ID'; // Replace with your Agora App ID
   static const String token = ''; // Replace with your Token (or keep empty for testing)
   static const String channelName = 'test_channel'; // Replace with your desired channel name
 
